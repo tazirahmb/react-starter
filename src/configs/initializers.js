f137b21e88be187d1b84bb21bb09df154604a854
Tazirahmb <tazirahmb@outlook.com>
2021-05-09 13:45:44 +0700

    add initailizer for dev mode

---
+++ src/configs/initializers.js
@@ -0,0 +1,19 @@
+// set base url for API based on MODE
+let BASE_URL;
+switch (process.env.MODE) {
+  case 'production':
+    BASE_URL = 'https://insert-production-url-here';
+    break;
+  case 'staging':
+    BASE_URL = 'http://insert-stage-url-here';
+    break;
+  default:
+    BASE_URL = 'http://insert-development-url-here';
+    break;
+}
+
+const initializers = {
+  BASE_URL,
+};
+
+export default initializers;
